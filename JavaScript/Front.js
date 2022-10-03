ab01ae62fc32a9bc439bfe094b64e8429885bf64
pabloramonhernandez <pabloramonherdez@gmail.com>
2022-10-03 12:22:54 -0400

    Adding Services and Contact pages

--- JavaScript/Front.js
+++ JavaScript/Front.js
@@ -1,9 +1,12 @@
 const name = 'John';
 const age = '30';
-const rating = 4.5;
-const isCool = true;
-const x = null;
-const y = undefined;
-let z;
 
-console.log(typeof age);
+
+console.log('My name is ' + name +' and I am ' + age);
+
+const hello = (`My name is ${name} and I am ${age}`);
+
+console.log(hello);
+
+const s = 'Good Bye'
+console.log(s.substring(0,5).toUpperCase());
